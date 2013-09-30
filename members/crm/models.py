ede7982aea957e747187943aa7786bebeda652fe
Jure Cuhalev <gandalfar@gmail.com>
2013-09-30 22:07:14 +0200

    fix indent

--- members/crm/models.py
+++ members/crm/models.py
@@ -11,162 +11,162 @@ class Country(models.Model):
     developing = models.BooleanField()
 
     class Meta:
-    	ordering = ('name',)
+        ordering = ('name',)
 
     def __unicode__(self):
-    	return self.name
+        return self.name
 
 ORGANIZATION_MEMBERSHIP_TYPE_CHOICES = (
-	(5 , 'Institutional Members'),
-	(6 , 'Organizational Members'),
-	(7 , 'Associate Consortium Members'),
-	(8 , 'Corporate Members - Basic'),
-	(9 , 'Associate Institutional Members'),
-	(10, 'Institutional Members - MRC'),
-	(11, 'Institutional Members - DC'),
-	(12, 'Institutional Members - DC - MRC'),
-	(13, 'Organizational Members - DC'),
-	(14, 'Associate Consortium Members - DC'),
-	(15, 'Corporate Members - Premium'),
-	(16, 'Corporate Members - Sustaining'),
-	(17, 'Associate Institutional Members - DC'),
-	(18, 'Nominating Committee')
+    (5 , 'Institutional Members'),
+    (6 , 'Organizational Members'),
+    (7 , 'Associate Consortium Members'),
+    (8 , 'Corporate Members - Basic'),
+    (9 , 'Associate Institutional Members'),
+    (10, 'Institutional Members - MRC'),
+    (11, 'Institutional Members - DC'),
+    (12, 'Institutional Members - DC - MRC'),
+    (13, 'Organizational Members - DC'),
+    (14, 'Associate Consortium Members - DC'),
+    (15, 'Corporate Members - Premium'),
+    (16, 'Corporate Members - Sustaining'),
+    (17, 'Associate Institutional Members - DC'),
+    (18, 'Nominating Committee')
 )
 
 ORGANIZATION_TYPE_CHOICES = (
-	('university', 'Higher Education Institution'),
-	('npo', 'Non-Profit Organization'),
-	('ngo', 'Non-Governmental Organization'),
-	('regionalconsortium', 'Regional Consortium'),
-	('software', 'Software Development'),
-	('commercial', 'Commercial Entity')
+    ('university', 'Higher Education Institution'),
+    ('npo', 'Non-Profit Organization'),
+    ('ngo', 'Non-Governmental Organization'),
+    ('regionalconsortium', 'Regional Consortium'),
+    ('software', 'Software Development'),
+    ('commercial', 'Commercial Entity')
 )
 
 ORGANIZATION_MEMBERSHIP_STATUS = (
-	(1,	'Applied'),
-	(2,	'Current'),
-	(3,	'Grace'),
-	(4,	'Expired'),
-	(5,	'Pending'),
-	(6,	'Cancelled'),
-	(7,	'Sustaining'),
-	# (8,	'Deceased'),
-	# (9,	'Testing'),
-	# (10,'Committee'),
-	# (11,'Committee'),
-	# (12,'Suspended'),
-	# (13,'New'),
-	(99,'Example')
+    (1, 'Applied'),
+    (2, 'Current'),
+    (3, 'Grace'),
+    (4, 'Expired'),
+    (5, 'Pending'),
+    (6, 'Cancelled'),
+    (7, 'Sustaining'),
+    # (8,   'Deceased'),
+    # (9,   'Testing'),
+    # (10,'Committee'),
+    # (11,'Committee'),
+    # (12,'Suspended'),
+    # (13,'New'),
+    (99,'Example')
 )
 
 ORGANIZATION_ASSOCIATED_CONSORTIUM = (
-	('CCCOER', 'CCCOER'),
-	('CORE', 'CORE'),
-	('JOCWC', 'JOCWC'),
-	('KOCWC', 'KOCWC'),
-	('TOCWC', 'TOCWC'),
-	('Turkish OCWC', 'Turkish OCWC'),
-	('UNIVERSIA', 'UNIVERSIA')
+    ('CCCOER', 'CCCOER'),
+    ('CORE', 'CORE'),
+    ('JOCWC', 'JOCWC'),
+    ('KOCWC', 'KOCWC'),
+    ('TOCWC', 'TOCWC'),
+    ('Turkish OCWC', 'Turkish OCWC'),
+    ('UNIVERSIA', 'UNIVERSIA')
 )
 
 class ActiveOrganizationManager(models.Manager):
-	def get_query_set(self):
-		return super(ActiveOrganizationManager, self).get_query_set().filter(membership_status__in=(2,3,7)).order_by('display_name')
+    def get_query_set(self):
+        return super(ActiveOrganizationManager, self).get_query_set().filter(membership_status__in=(2,3,7)).order_by('display_name')
 
 class Organization(models.Model):
-	legal_name = models.CharField(max_length=255, blank=True)
-	display_name = models.CharField(max_length=255, verbose_name="Name of the organization")
-	slug = models.CharField(max_length=30, unique=True, default='')
-	user = models.ForeignKey(User, blank=True, null=True)
+    legal_name = models.CharField(max_length=255, blank=True)
+    display_name = models.CharField(max_length=255, verbose_name="Name of the organization")
+    slug = models.CharField(max_length=30, unique=True, default='')
+    user = models.ForeignKey(User, blank=True, null=True)
 
-	membership_type = models.IntegerField(max_length=10, choices=ORGANIZATION_MEMBERSHIP_TYPE_CHOICES)
-	# organization_type = models.CharField(max_length=255, choices=ORGANIZATION_TYPE_CHOICES)
-	membership_status = models.IntegerField(max_length=10, choices=ORGANIZATION_MEMBERSHIP_STATUS)
-	associate_consortium = models.CharField(max_length=255, choices=ORGANIZATION_ASSOCIATED_CONSORTIUM, blank=True, default='')
+    membership_type = models.IntegerField(max_length=10, choices=ORGANIZATION_MEMBERSHIP_TYPE_CHOICES)
+    # organization_type = models.CharField(max_length=255, choices=ORGANIZATION_TYPE_CHOICES)
+    membership_status = models.IntegerField(max_length=10, choices=ORGANIZATION_MEMBERSHIP_STATUS)
+    associate_consortium = models.CharField(max_length=255, choices=ORGANIZATION_ASSOCIATED_CONSORTIUM, blank=True, default='')
 
-	crmid = models.CharField(max_length=255, blank=True, help_text='Legacy identifier')
+    crmid = models.CharField(max_length=255, blank=True, help_text='Legacy identifier')
 
-	main_website = models.TextField(max_length=255, blank=True)
-	ocw_website = models.TextField(max_length=255, blank=True, verbose_name="OCW Website")
+    main_website = models.TextField(max_length=255, blank=True)
+    ocw_website = models.TextField(max_length=255, blank=True, verbose_name="OCW Website")
 
-	description = models.TextField(blank=True)
-	logo_large = models.ImageField(max_length=255, upload_to="logos", blank=True)
-	logo_small = models.ImageField(max_length=255, upload_to="logos", blank=True)
-	rss_course_feed = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True)
+    logo_large = models.ImageField(max_length=255, upload_to="logos", blank=True)
+    logo_small = models.ImageField(max_length=255, upload_to="logos", blank=True)
+    rss_course_feed = models.CharField(max_length=255, blank=True)
 
-	accreditation_body = models.CharField(max_length=255, blank=True, default='')
-	support_commitment = models.TextField(blank=True, default='')
+    accreditation_body = models.CharField(max_length=255, blank=True, default='')
+    support_commitment = models.TextField(blank=True, default='')
 
-	objects = models.Manager()
-	active = ActiveOrganizationManager()
+    objects = models.Manager()
+    active = ActiveOrganizationManager()
 
-	def __unicode__(self):
-		return self.display_name
+    def __unicode__(self):
+        return self.display_name
 
-	def get_absolute_staff_url(self):
-		return reverse('staff:organization-view', kwargs={'pk':self.id})
+    def get_absolute_staff_url(self):
+        return reverse('staff:organization-view', kwargs={'pk':self.id})
 
-	def get_absolute_url(self):
-		return reverse('crm:organization-view', kwargs={'pk':self.id})
+    def get_absolute_url(self):
+        return reverse('crm:organization-view', kwargs={'pk':self.id})
 
-	def save(self, force_insert=False, force_update=False, using=None):
-		if not self.slug:
-			slug = slugify(self.display_name)[:30]
-			if Organization.objects.filter(slug=slug).exists():
-				slug = slug[:29] + '2'
-			self.slug = slug
+    def save(self, force_insert=False, force_update=False, using=None):
+        if not self.slug:
+            slug = slugify(self.display_name)[:30]
+            if Organization.objects.filter(slug=slug).exists():
+                slug = slug[:29] + '2'
+            self.slug = slug
 
-		super(Organization, self).save(force_insert=force_insert, force_update=force_update, using=using)
+        super(Organization, self).save(force_insert=force_insert, force_update=force_update, using=using)
 
 reversion.register(Organization)
 
 # CONTACT_TYPE_CHOICES = (
-# 	('lead', 'Main contact'),
-# 	('tech', 'Technical contact'),
-# 	('billing', 'Billing contact')
+#   ('lead', 'Main contact'),
+#   ('tech', 'Technical contact'),
+#   ('billing', 'Billing contact')
 # )
 
 CONTACT_TYPE_CHOICES = (
-	(4,	 'Employee of'),
-	(6,	 'Lead Contact for'),
-	(9,	 'Certifier for'),
-	(10, 'Voting Representative'),
-	(11, 'Affiliated with'),
-	(12, 'AC Member of'),
+    (4,  'Employee of'),
+    (6,  'Lead Contact for'),
+    (9,  'Certifier for'),
+    (10, 'Voting Representative'),
+    (11, 'Affiliated with'),
+    (12, 'AC Member of'),
 )
 
 class Contact(models.Model):
-	organization = models.ForeignKey(Organization)
-	
-	contact_type = models.IntegerField(max_length=10, choices=CONTACT_TYPE_CHOICES)
-	email = models.EmailField(max_length=255)
+    organization = models.ForeignKey(Organization)
+    
+    contact_type = models.IntegerField(max_length=10, choices=CONTACT_TYPE_CHOICES)
+    email = models.EmailField(max_length=255)
 
-	first_name = models.CharField(max_length=255, blank=True, default='')
-	last_name = models.CharField(max_length=255, blank=True, default='')
-	job_title = models.CharField(max_length=255, blank=True, default='')
+    first_name = models.CharField(max_length=255, blank=True, default='')
+    last_name = models.CharField(max_length=255, blank=True, default='')
+    job_title = models.CharField(max_length=255, blank=True, default='')
 reversion.register(Contact)
 
 class Address(models.Model):
-	organization = models.ForeignKey(Organization)
+    organization = models.ForeignKey(Organization)
 
-	street_address = models.CharField(max_length=255, blank=True, help_text='Street address with street number')
-	supplemental_address_1 = models.CharField(max_length=255, blank=True)
-	supplemental_address_2 = models.CharField(max_length=255, blank=True)
-	
-	city = models.CharField(max_length=255, blank=True)
-	postal_code = models.CharField(max_length=50, blank=True)
-	postal_code_suffix = models.CharField(max_length=255, blank=True)
-	
-	state_province = models.CharField(max_length=255, blank=True)
-	state_province_abbr = models.CharField(max_length=255, blank=True)
+    street_address = models.CharField(max_length=255, blank=True, help_text='Street address with street number')
+    supplemental_address_1 = models.CharField(max_length=255, blank=True)
+    supplemental_address_2 = models.CharField(max_length=255, blank=True)
+    
+    city = models.CharField(max_length=255, blank=True)
+    postal_code = models.CharField(max_length=50, blank=True)
+    postal_code_suffix = models.CharField(max_length=255, blank=True)
+    
+    state_province = models.CharField(max_length=255, blank=True)
+    state_province_abbr = models.CharField(max_length=255, blank=True)
 
-	country = models.ForeignKey(Country, blank=True, null=True)
+    country = models.ForeignKey(Country, blank=True, null=True)
 
-	latitude = models.FloatField(blank=True, null=True)
-	longitude = models.FloatField(blank=True, null=True)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
 
-	def __unicode__(self):
-		return u"%s %s %s" % (self.country.name, self.city, self.street_address)
+    def __unicode__(self):
+        return u"%s %s %s" % (self.country.name, self.city, self.street_address)
 
 reversion.register(Address)
 
@@ -175,9 +175,9 @@ APPLICATION_MEMBERSHIP_TYPE = (
 )
 
 APPLICATION_STATUS_CHOICES = (
-	('Approved', 'Approved'),
-	('Rejected', 'Rejected'),
-	('Spam', 'Spam'),
+    ('Approved', 'Approved'),
+    ('Rejected', 'Rejected'),
+    ('Spam', 'Spam'),
 )
 
 class MembershipApplication(models.Model):
@@ -217,7 +217,7 @@ class MembershipApplication(models.Model):
     organization_type = models.CharField(max_length=765, blank=True)
 
     accreditation_body = models.CharField(max_length=765, blank=True, default='')
-    ocw_launch_date = models.DateTimeField(null=True, blank=True)	
+    ocw_launch_date = models.DateTimeField(null=True, blank=True)   
 
     support_commitment = models.TextField(blank=True)
 
@@ -226,37 +226,37 @@ class MembershipApplication(models.Model):
     modified = models.DateTimeField() #auto_now=True
 
 COMMENTS_APP_STATUS_CHOICES = (
-	('Requested More Info', 'Requested More Info'),
-	('Spam', 'Spam'),
-	('Approved', 'Approved')
+    ('Requested More Info', 'Requested More Info'),
+    ('Spam', 'Spam'),
+    ('Approved', 'Approved')
 )
 
 class MembershipApplicationComment(models.Model):
-	application = models.ForeignKey(MembershipApplication)
+    application = models.ForeignKey(MembershipApplication)
 
-	legacy_comment_id = models.IntegerField(blank=True)
-	legacy_app_id = models.IntegerField(blank=True)
+    legacy_comment_id = models.IntegerField(blank=True)
+    legacy_app_id = models.IntegerField(blank=True)
 
-	comment = models.TextField(blank=True)
-	sent_email = models.BooleanField(default=False)
-	app_status = models.CharField(max_length=255, blank=True) #, choices=COMMENTS_APP_STATUS_CHOICES, blank=True)
+    comment = models.TextField(blank=True)
+    sent_email = models.BooleanField(default=False)
+    app_status = models.CharField(max_length=255, blank=True) #, choices=COMMENTS_APP_STATUS_CHOICES, blank=True)
 
-	created = models.DateTimeField() #auto_now_add=True
+    created = models.DateTimeField() #auto_now_add=True
 
 class ReportedStatistic(models.Model):
-	organization = models.ForeignKey(Organization)
-	report_month = models.CharField(max_length=6)
-	report_year = models.CharField(max_length=12)
-	site_visits = models.IntegerField()
-	orig_courses = models.IntegerField()
-	trans_courses = models.IntegerField()
-	orig_course_lang = models.TextField(blank=True)
-	trans_course_lang = models.TextField(blank=True, null=True)
+    organization = models.ForeignKey(Organization)
+    report_month = models.CharField(max_length=6)
+    report_year = models.CharField(max_length=12)
+    site_visits = models.IntegerField()
+    orig_courses = models.IntegerField()
+    trans_courses = models.IntegerField()
+    orig_course_lang = models.TextField(blank=True)
+    trans_course_lang = models.TextField(blank=True, null=True)
 
-	oer_resources = models.IntegerField(null=True, blank=True)
-	trans_oer_resources = models.IntegerField(null=True, blank=True)
-	comment = models.TextField(blank=True, null=True)
+    oer_resources = models.IntegerField(null=True, blank=True)
+    trans_oer_resources = models.IntegerField(null=True, blank=True)
+    comment = models.TextField(blank=True, null=True)
 
-	report_date = models.DateField()
-	last_modified = models.DateTimeField()
-	carry_forward = models.BooleanField()
+    report_date = models.DateField()
+    last_modified = models.DateTimeField()
+    carry_forward = models.BooleanField()
