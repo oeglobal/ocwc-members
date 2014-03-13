9dea7219b4ef5300863436995580a738f5de98cc
Jure Cuhalev <gandalfar@gmail.com>
2014-03-13 22:31:06 +0100

    fix api

--- members/elections/api.py
+++ members/elections/api.py
@@ -5,9 +5,9 @@ from .models import Candidate, Election
 from .serializers import CandidatePublicSerializer
 
 class ElectionCandidatesListAPIView(generics.ListAPIView):
-    model = Candidate
-    serializer_class = CandidatePublicSerializer
+	model = Candidate
+	serializer_class = CandidatePublicSerializer
 
-    def get_queryset(self):
-    	view_nominations_key = self.kwargs.pop('key')
-    	return Election.objects.get(view_nominations_key=view_nominations_key).candidate_set.filter(status='accepted')
+	def get_queryset(self):
+		view_nominations_key = self.kwargs.pop('key')
+		return Election.objects.get(view_nominations_key=view_nominations_key).candidate_set.filter(vetted=True).order_by('candidate_last_name')
