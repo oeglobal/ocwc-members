dbf48c0f7fbad25e269830e390daee507444a471
Jure Cuhalev <gandalfar@gmail.com>
2016-01-21 12:38:32 +0100

    count activie countries in admin

--- members/crm/admin.py
+++ members/crm/admin.py
@@ -3,110 +3,113 @@ from django.utils.html import format_html
 from django import forms
 
 from .models import Organization, Contact, Address, MembershipApplication, \
-					Country, ReportedStatistic, Invoice, \
-					BillingLog, LoginKey, Continent
+                    Country, ReportedStatistic, Invoice, \
+                    BillingLog, LoginKey, Continent
 
 class ContactInline(admin.TabularInline):
-	model = Contact
-	extra = 1
+    model = Contact
+    extra = 1
 
 class AddressInline(admin.StackedInline):
-	model = Address
-	extra = 1
+    model = Address
+    extra = 1
 
 class OrganizationAdmin(admin.ModelAdmin):
-	list_filter = ('membership_type', 'membership_status', 'ocw_contact')
-	list_display = ('display_name', 'associate_consortium', 'membership_status')
-	search_fields = ('display_name',)
-	inlines = [
-		ContactInline,
-		AddressInline
-	]
-	fieldsets = (
-		('General', {
-			'fields': ('display_name', 'membership_type', 'membership_status', 'associate_consortium','ocw_contact')
-		}),
-		('Websites', {
-			'fields': ('main_website', 'ocw_website'),
-		}),
-		('Additional', {
-			'fields': ('legal_name', 'user', 'slug', 'crmid', 'description', 'logo_large', 'logo_small', 'accreditation_body', 'support_commitment',)
-		})
-	)
+    list_filter = ('membership_type', 'membership_status', 'ocw_contact')
+    list_display = ('display_name', 'associate_consortium', 'membership_status')
+    search_fields = ('display_name',)
+    inlines = [
+        ContactInline,
+        AddressInline
+    ]
+    fieldsets = (
+        ('General', {
+            'fields': ('display_name', 'membership_type', 'membership_status', 'associate_consortium','ocw_contact')
+        }),
+        ('Websites', {
+            'fields': ('main_website', 'ocw_website'),
+        }),
+        ('Additional', {
+            'fields': ('legal_name', 'user', 'slug', 'crmid', 'description', 'logo_large', 'logo_small', 'accreditation_body', 'support_commitment',)
+        })
+    )
 
 class AddressAdmin(admin.ModelAdmin):
-	list_display = ('address_type', 'organization', 'street_address', 'city', 'country', 'latitude', 'longitude')
+    list_display = ('address_type', 'organization', 'street_address', 'city', 'country', 'latitude', 'longitude')
 
 class ContactAdmin(admin.ModelAdmin):
-	list_display = ('email', 'first_name', 'last_name', 'organization_link', 'contact_type')
-	search_fields = ('email', 'first_name', 'last_name')
+    list_display = ('email', 'first_name', 'last_name', 'organization_link', 'contact_type')
+    search_fields = ('email', 'first_name', 'last_name')
 
-	def organization_link(self, obj):
-		return format_html('<a href="/admin/crm/organization/%s/">%s</a>' % (obj.organization.id, obj.organization))
-	organization_link.allow_tags = True
+    def organization_link(self, obj):
+        return format_html('<a href="/admin/crm/organization/%s/">%s</a>' % (obj.organization.id, obj.organization))
+    organization_link.allow_tags = True
 
 class MembershipApplicationForm(forms.ModelForm):
-	class Meta:
-		model = MembershipApplication
+    class Meta:
+        model = MembershipApplication
 
-	def __init__(self, *args, **kwargs):
-		super(MembershipApplicationForm, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(MembershipApplicationForm, self).__init__(*args, **kwargs)
 
-	def clean_membership_type(self):
-		app_status = self.cleaned_data.get('app_status')
-		membership_type = self.cleaned_data.get('membership_type')
+    def clean_membership_type(self):
+        app_status = self.cleaned_data.get('app_status')
+        membership_type = self.cleaned_data.get('membership_type')
 
-		if app_status == 'Approved':
-			if not membership_type:
-				raise forms.ValidationError('Please set Membership Type')
+        if app_status == 'Approved':
+            if not membership_type:
+                raise forms.ValidationError('Please set Membership Type')
 
-		return membership_type
+        return membership_type
 
 class MembershipApplicationAdmin(admin.ModelAdmin):
-	list_display = ('id', 'display_name', 'organization' , 'membership_type', 'legacy_application_id', 'main_website')
-	list_filter = ('app_status',)
-	search_fields = ('display_name', 'description')
-	raw_id_fields = ('organization',)
-	fieldsets = (
-		(None, {
-			'fields': ('app_status', 'display_name', 'description', 'membership_type', 'organization')
-			}
-		),
-		('General', {
-			'fields': ('organization_type', 'main_website', 'ocw_website', 'institution_country',
-						'rss_course_feed', 'is_accredited', 'accreditation_body', 'support_commitment')
-		}),
-		('Membership', {
-			'fields': ('simplified_membership_type', 'corporate_support_levels', 'associate_consortium')
-		}),
-		('Address and Contact', {
-			'fields': ('first_name', 'last_name', 'email', 'job_title',
-						'street_address', 'supplemental_address_1', 'supplemental_address_2', 'city', 'postal_code',
-						'state_province', 'country', )
-		})
-	)
-	form = MembershipApplicationForm
+    list_display = ('id', 'display_name', 'organization' , 'membership_type', 'legacy_application_id', 'main_website')
+    list_filter = ('app_status',)
+    search_fields = ('display_name', 'description')
+    raw_id_fields = ('organization',)
+    fieldsets = (
+        (None, {
+            'fields': ('app_status', 'display_name', 'description', 'membership_type', 'organization')
+            }
+        ),
+        ('General', {
+            'fields': ('organization_type', 'main_website', 'ocw_website', 'institution_country',
+                        'rss_course_feed', 'is_accredited', 'accreditation_body', 'support_commitment')
+        }),
+        ('Membership', {
+            'fields': ('simplified_membership_type', 'corporate_support_levels', 'associate_consortium')
+        }),
+        ('Address and Contact', {
+            'fields': ('first_name', 'last_name', 'email', 'job_title',
+                        'street_address', 'supplemental_address_1', 'supplemental_address_2', 'city', 'postal_code',
+                        'state_province', 'country', )
+        })
+    )
+    form = MembershipApplicationForm
 
 class MembershipApplicationCommentAdmin(admin.ModelAdmin):
-	list_display = ('application', 'legacy_comment_id', 'legacy_app_id', 'comment', 'app_status')
-	list_filter = ('app_status',)
+    list_display = ('application', 'legacy_comment_id', 'legacy_app_id', 'comment', 'app_status')
+    list_filter = ('app_status',)
 
 class CountryAdmin(admin.ModelAdmin):
-	list_display = ('name', 'iso_code', 'developing')
-	list_filter = ('developing',)
+    list_display = ('name', 'iso_code', 'developing', 'active_count')
+    list_filter = ('developing',)
+
+    def active_count(self, obj):
+        return obj.address_set.filter(organization__membership_status__in=(2,3,5,7,99)).count()
 
 class ReportedStatisticAdmin(admin.ModelAdmin):
-	list_display = ('organization', 'last_modified', 'site_visits', 'orig_courses',
-					'trans_courses', 'orig_course_lang', 'trans_course_lang',
-					'oer_resources', 'trans_oer_resources')
-	search_fields = ('organization__display_name',)
-	# list_filter = ('report_year',)
+    list_display = ('organization', 'last_modified', 'site_visits', 'orig_courses',
+                    'trans_courses', 'orig_course_lang', 'trans_course_lang',
+                    'oer_resources', 'trans_oer_resources')
+    search_fields = ('organization__display_name',)
+    # list_filter = ('report_year',)
 
 class InvoiceAdmin(admin.ModelAdmin):
-	list_display = ('invoice_number', 'organization', 'pub_date')
+    list_display = ('invoice_number', 'organization', 'pub_date')
 
 class BillingLogAdmin(admin.ModelAdmin):
-	list_display = ('log_type', 'organization', 'pub_date')
+    list_display = ('log_type', 'organization', 'pub_date')
 
 admin.site.register(Organization, OrganizationAdmin)
 admin.site.register(Contact, ContactAdmin)
