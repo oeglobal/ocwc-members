1980edd95e96d2f88e8004db07a394fce75e321d
Jure Cuhalev <jure@cuhalev.com>
2017-01-09 15:51:23 +0100

    Fix for hanging phantomjs

--- members/conferences/views.py
+++ members/conferences/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import uuid
-import subprocess
+import subprocess2
 
 from django.shortcuts import redirect
 from django.contrib import messages
@@ -44,14 +44,14 @@ class InvoicePDF(StaffuserRequiredMixin, TemplateView):
         url = '{}{}{}'.format(settings.INVOICES_PHANTOM_JS_HOST, registration.get_access_key_url(), paid_queryparam)
 
         pdf = '/tmp/oeglobal_invoice_{}.pdf'.format(uuid.uuid4().get_hex())
-        popen_instance = subprocess.Popen([here('../../bin/phantomjs'),
+        popen_instance = subprocess2.Popen([here('../../bin/phantomjs'),
                           here('../crm/phantomjs-scripts/rasterize.js'),
                           url,
                           pdf,
                           'Letter'
                         ])
 
-        subprocess.Popen.wait(popen_instance)
+        subprocess2.Popen.waitOrTerminate(popen_instance, timeoutSeconds=30)
 
         with open(pdf, 'r') as pdf_file:
             response = HttpResponse(pdf_file.read(), mimetype='application/pdf')
