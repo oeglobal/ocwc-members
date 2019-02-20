3e5e35aa85e656efbdddddf4c4d2accad964a42b
Jure Cuhalev <jure@cuhalev.com>
2019-02-20 14:16:57 +0100

    Update elections with new apis

--- members/elections/serializers.py
+++ members/elections/serializers.py
@@ -2,9 +2,16 @@ from rest_framework import serializers
 
 from .models import Election, Candidate
 
+
 class CandidatePublicSerializer(serializers.ModelSerializer):
-	organization = serializers.CharField(source='organization.display_name')
-	class Meta:
-		model = Candidate
-		fields = ('candidate_first_name', 'candidate_last_name', 'candidate_job_title', 
-				  'biography', 'vision', 'ideas', 'expertise', 'external_url', 'seat_type', 'organization', 'reason')
+    organization = serializers.CharField(source='organization.display_name')
+    expertise = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Candidate
+        fields = ('candidate_first_name', 'candidate_last_name', 'candidate_job_title',
+                  'biography', 'vision', 'ideas', 'expertise', 'expertise_other', 'expertise_expanded',
+                  'external_url', 'seat_type', 'organization', 'reason')
+
+    def get_expertise(self, obj):
+        return ', '.join(obj.get_expertise_items())
