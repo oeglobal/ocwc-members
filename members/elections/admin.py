fc7d88d85d989ab7946981101f6f8df7eadb7d34
Jure Cuhalev <jure@cuhalev.com>
2019-03-14 10:58:47 +0100

    Complete 2019 elections

--- members/elections/admin.py
+++ members/elections/admin.py
@@ -19,8 +19,8 @@ class CandidateAdmin(admin.ModelAdmin):
     list_display = ['organization', 'candidate_first_name', 'candidate_last_name', 'status', 'vetted', 'seat_type', ]
     list_filter = ['election', 'vetted', 'status']
 
-# admin.site.register(Election)
-# admin.site.register(Candidate, CandidateAdmin)
-# admin.site.register(CandidateBallot, CandidateBallotAdmin)
-# admin.site.register(Proposition)
-# admin.site.register(PropositionBallot, PropositionBallotAdmin)
+admin.site.register(Election)
+admin.site.register(Candidate, CandidateAdmin)
+admin.site.register(CandidateBallot, CandidateBallotAdmin)
+admin.site.register(Proposition)
+admin.site.register(PropositionBallot, PropositionBallotAdmin)
