e17c41051c1de372356eed642431f32e0ca3eddd
Jure Cuhalev <jure@cuhalev.com>
2020-01-14 22:30:43 +0700

    Add missing migration

--- members/crm/migrations/0028_auto_20200114_0921.py
+++ members/crm/migrations/0028_auto_20200114_0921.py
@@ -7,19 +7,52 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('crm', '0027_auto_20190920_0824'),
-    ]
+    dependencies = [("crm", "0027_auto_20190920_0824")]
 
     operations = [
         migrations.AlterField(
-            model_name='membershipapplication',
-            name='institution_type',
-            field=models.CharField(blank=True, choices=[(b'secondary-ed', b'Primary and secondary (K-12)'), (b'college', b'Community, technical, or vocational college'), (b'higher-ed', b'University'), (b'non-accredited', b'Informal non-accredited education'), (b'lifelong', b'Lifelong learning'), (b'consortium', b'Consortia'), (b'initiative', b'Open initiatives or special project'), (b'commercial', b'Corporate enterprise'), (b'npo', b'Non-profits, NGO\xe2\x80\x99s, IGO'), (b'cultural', b'Cultural organization'), (b'gov', b'Government')], default=b'', max_length=25, verbose_name=b'Institution Category'),
+            model_name="membershipapplication",
+            name="institution_type",
+            field=models.CharField(
+                blank=True,
+                choices=[
+                    (b"secondary-ed", b"Primary and secondary (K-12)"),
+                    (b"college", b"Community, technical, or vocational college"),
+                    (b"higher-ed", b"University"),
+                    (b"non-accredited", b"Informal non-accredited education"),
+                    (b"lifelong", b"Lifelong learning"),
+                    (b"consortium", b"Consortia"),
+                    (b"initiative", b"Open initiatives or special project"),
+                    (b"commercial", b"Corporate enterprise"),
+                    (b"npo", b"Non-profits, NGO\xe2\x80\x99s, IGO"),
+                    (b"cultural", b"Cultural organization"),
+                    (b"gov", b"Government"),
+                ],
+                default=b"",
+                max_length=25,
+                verbose_name=b"Institution Category",
+            ),
         ),
         migrations.AlterField(
-            model_name='organization',
-            name='institution_type',
-            field=models.CharField(blank=True, choices=[(b'secondary-ed', b'Primary and secondary (K-12)'), (b'college', b'Community, technical, or vocational college'), (b'higher-ed', b'University'), (b'non-accredited', b'Informal non-accredited education'), (b'lifelong', b'Lifelong learning'), (b'consortium', b'Consortia'), (b'initiative', b'Open initiatives or special project'), (b'commercial', b'Corporate enterprise'), (b'npo', b'Non-profits, NGO\xe2\x80\x99s, IGO'), (b'cultural', b'Cultural organization'), (b'gov', b'Government')], default=b'', max_length=25),
+            model_name="organization",
+            name="institution_type",
+            field=models.CharField(
+                blank=True,
+                choices=[
+                    (b"secondary-ed", b"Primary and secondary (K-12)"),
+                    (b"college", b"Community, technical, or vocational college"),
+                    (b"higher-ed", b"University"),
+                    (b"non-accredited", b"Informal non-accredited education"),
+                    (b"lifelong", b"Lifelong learning"),
+                    (b"consortium", b"Consortia"),
+                    (b"initiative", b"Open initiatives or special project"),
+                    (b"commercial", b"Corporate enterprise"),
+                    (b"npo", b"Non-profits, NGO\xe2\x80\x99s, IGO"),
+                    (b"cultural", b"Cultural organization"),
+                    (b"gov", b"Government"),
+                ],
+                default=b"",
+                max_length=25,
+            ),
         ),
     ]
